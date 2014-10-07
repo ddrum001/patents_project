32c55996e4a2389d6f6b280efa4ed1db3b567879
ddrum001 <ddrum001@gmail.com>
2014-10-07 09:03:27 +0000

    more robust parallel ingestion script

--- par_ingest/parallel_parse.sh
+++ par_ingest/parallel_parse.sh
@@ -4,17 +4,29 @@
 # case with just the name node parsing, and seems unavoidable given
 # the fact that the xml records span a variable number of lines
 
-# starting_year is the year when txt files stopped
+# first we find all the file that need to be processed in the hdfs
+# and store the names in a  file.  It sorts them by file size (the 5th column) and
+# prints only the file path (the 8th column)
 
-current_year=$(date +%Y)
-starting_year=2001
-total_years=$((current_year-starting_year))
+hdfs dfs -ls /user/ubuntu/data/raw_patents | sort -k +5n | awk '{print $8}' > files_to_be_parsed.txt
 
 # this command finds and stores the ip's of the worker nodes
 
 sudo -u hdfs hdfs dfsadmin -report | grep ^Name | cut -f3 -d ' ' | tr -d '(' | tr -d ')' > worker_ips.txt
 number_of_nodes=$(($(wc -l worker_ips.txt | cut -f1 -d ' ')+1))
 
-# work can be distributed to the worker nodes by generating and moving all the necessary scripts with
-for 
-scp -i ~/.ssh/insight-cloudera.pem work ubuntu@ip-172-31-26-16.us-west-1.compute.internal:/home/ubuntu/
+# work can be distributed to the worker nodes by passing a list of files to be copied to
+# the linux file system of the worker nodes by splitting the files into equal pieces
+
+for ((node=1; node<$number_of_nodes; node++))
+do
+	file=node_files_$node.sh
+	echo mkdir -p /home/ubuntu/data/raw_patents > $file
+	echo hdfs dfs -get $(awk "NR % $number_of_nodes == $node" files_to_be_parsed.txt) /home/ubuntu/data/raw_patents/ >> $file
+	echo scp -i ~/.ssh/insight-cloudera.pem ./$file ubuntu@$(awk "NR % $number_of_nodes == $node" worker_ips.txt):/home/ubuntu/ 
+done
+
+# scripts can be ran using tmux and ssh with
+
+# tmux -s node1 -d
+# tmux send-keys "./$file" Enter
